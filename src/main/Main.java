092f9b9fde77cfc13077bf02b45936c15c5eb580
Dmytro Obolkin <obolkindm@gmail.com>
2023-04-19 13:16:23 +0300

    Updated cycle in Main class.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package src.main;
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 3; i ++){
+        for (int i = 0; i <= 3; i ++){
             System.out.print("Hello, world!");
         }
     }
